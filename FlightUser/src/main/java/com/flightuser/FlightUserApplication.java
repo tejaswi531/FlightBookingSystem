6749b454022ab9388d8f3368f01fcd1003ad279e
tejaswi531 <54668200+tejaswi531@users.noreply.github.com>
2022-07-26 12:21:02 +0530

    Add files via upload

---
+++ FlightUser/src/main/java/com/flightuser/FlightUserApplication.java
@@ -0,0 +1,13 @@
+package com.flightuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightUserApplication.class, args);
+	}
+
+}
