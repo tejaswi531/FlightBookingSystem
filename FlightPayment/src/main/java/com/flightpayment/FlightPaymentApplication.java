1faac9676c0934422232dacbcd5a5a2eca3cfac1
tejaswi531 <54668200+tejaswi531@users.noreply.github.com>
2022-07-26 12:20:23 +0530

    Add files via upload

---
+++ FlightPayment/src/main/java/com/flightpayment/FlightPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.flightpayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightPaymentApplication.class, args);
+	}
+
+}
---
+++ FlightPayment/src/test/java/com/flightpayment/FlightPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flightpayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
