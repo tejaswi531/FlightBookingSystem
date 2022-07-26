d63b5cef393dc1802ace82675673c92a1ca9395d
tejaswi531 <54668200+tejaswi531@users.noreply.github.com>
2022-07-26 12:07:07 +0530

    Add files via upload

---
+++ FlightAdmin/src/main/java/com/flightadmin/FlightAdminApplication.java
@@ -0,0 +1,13 @@
+package com.flightadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightAdminApplication.class, args);
+	}
+
+}
